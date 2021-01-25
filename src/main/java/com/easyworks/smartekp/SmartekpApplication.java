ecb0f1a2411d4298c378be0a318f4ca5a1267801
kantabest <kantabest@gmail.com>
2021-01-26 08:45:34 +0900

    20210125

--- src/main/java/com/easyworks/smartekp/SmartekpApplication.java
+++ src/main/java/com/easyworks/smartekp/SmartekpApplication.java
@@ -1,7 +1,12 @@
 package com.easyworks.smartekp;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+
+
+@EnableAutoConfiguration(exclude={DataSourceAutoConfiguration.class})
 
 @SpringBootApplication
 public class SmartekpApplication {
